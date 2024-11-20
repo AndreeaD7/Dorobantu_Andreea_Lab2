33a72844d03c1e6a34aa32966d159517725d758d
AndreeaD7 <andreea.dorobantu98@gmail.com>
2024-11-20 15:55:58 +0200

    Add project files.

---
+++ Data/Dorobantu_Andreea_Lab2Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Dorobantu_Andreea_Lab2.Models;
+
+namespace Dorobantu_Andreea_Lab2.Data
+{
+    public class Dorobantu_Andreea_Lab2Context : DbContext
+    {
+        public Dorobantu_Andreea_Lab2Context (DbContextOptions<Dorobantu_Andreea_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Dorobantu_Andreea_Lab2.Models.Book> Book { get; set; } = default!;
+        public DbSet<Dorobantu_Andreea_Lab2.Models.Publisher> Publisher { get; set; } = default!;
+    }
+}
---
+++ Pages/Publishers/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Dorobantu_Andreea_Lab2.Data;
+using Dorobantu_Andreea_Lab2.Models;
+
+namespace Dorobantu_Andreea_Lab2.Pages.Publishers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Dorobantu_Andreea_Lab2.Data.Dorobantu_Andreea_Lab2Context _context;
+
+        public IndexModel(Dorobantu_Andreea_Lab2.Data.Dorobantu_Andreea_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Publisher> Publisher { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Publisher = await _context.Publisher.ToListAsync();
+        }
+    }
+}
