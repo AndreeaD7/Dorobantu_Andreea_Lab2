5f62304e1e552c3c490a9e28cde54aec8db49e1a
AndreeaD7 <andreea.dorobantu98@gmail.com>
2024-11-20 22:16:47 +0200

    Lab4

---
+++ Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,9 @@
+﻿using Dorobantu_Andreea_Lab2.Models;
+namespace Dorobantu_Andreea_Lab2.Models.ViewModels
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
--- Pages/Publishers/Index.cshtml.cs
+++ Pages/Publishers/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Dorobantu_Andreea_Lab2.Data;
 using Dorobantu_Andreea_Lab2.Models;
+using Dorobantu_Andreea_Lab2.Models.ViewModels;
 
 namespace Dorobantu_Andreea_Lab2.Pages.Publishers
 {
@@ -19,16 +20,29 @@ namespace Dorobantu_Andreea_Lab2.Pages.Publishers
             _context = context;
         }
 
-        public IList<Publisher> Publisher { get; set; } //= default!;
-        /*public IList<Book> Book { get; set; }
-        public BookData BookD { get; set; }
+        public IList<Publisher> Publisher { get; set; } = default!;
+        public PublisherIndexData PublisherData { get; set; }
+        public int PublisherID { get; set; }
         public int BookID { get; set; }
-        public int CategoryID { get; set; }*/
 
         public async Task OnGetAsync(int? id, int? categoryID)
         {
-            Publisher = await _context.Publisher.ToListAsync();
-            /*BookD = new BookData();
+            PublisherData = new PublisherIndexData();
+            PublisherData.Publishers = await _context.Publisher
+                .Include(i => i.Books)
+                    .ThenInclude(c => c.Author)
+                .OrderBy(i => i.PublisherName)
+                .ToListAsync();
+
+            if (id != null)
+            {
+                PublisherID = id.Value;
+                Publisher publisher = PublisherData.Publishers
+                    .Where(i => i.ID == id.Value).Single();
+                PublisherData.Books = publisher.Books;
+            }
+            /*Publisher = await _context.Publisher.ToListAsync();
+            BookD = new BookData();
 
             BookD.Books = await _context.Book
                   .Include(b => b.Publisher)
