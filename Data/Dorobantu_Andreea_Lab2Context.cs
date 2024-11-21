7cbb6962fdb614634b10399a2a175be0d41dfd06
AndreeaD7 <andreea.dorobantu98@gmail.com>
2024-11-21 14:21:57 +0200

    Lab5

--- Data/Dorobantu_Andreea_Lab2Context.cs
+++ Data/Dorobantu_Andreea_Lab2Context.cs
@@ -17,5 +17,7 @@ namespace Dorobantu_Andreea_Lab2.Data
         public DbSet<Dorobantu_Andreea_Lab2.Models.Book> Book { get; set; } = default!;
         public DbSet<Dorobantu_Andreea_Lab2.Models.Publisher> Publisher { get; set; } = default!;
         public DbSet<Dorobantu_Andreea_Lab2.Models.Category> Category { get; set; } = default!;
+        public DbSet<Dorobantu_Andreea_Lab2.Models.Member> Member { get; set; } = default!;
+        public DbSet<Dorobantu_Andreea_Lab2.Models.Borrowing> Borrowing { get; set; } = default!;
     }
 }
---
+++ Pages/Members/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Dorobantu_Andreea_Lab2.Data;
+using Dorobantu_Andreea_Lab2.Models;
+
+namespace Dorobantu_Andreea_Lab2.Pages.Members
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Dorobantu_Andreea_Lab2.Data.Dorobantu_Andreea_Lab2Context _context;
+
+        public CreateModel(Dorobantu_Andreea_Lab2.Data.Dorobantu_Andreea_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Member Member { get; set; } = default!;
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Member.Add(Member);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
